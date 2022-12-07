fdc2b206e979a4a556746dd4f10a3fb5075fb786
carpii <carpii.uk@gmail.com>
2022-12-07 17:55:06 +0000

    update selector used to identify loggedin/homepage

--- session.js
+++ session.js
@@ -51,9 +51,9 @@ class Session {
   }
 
   async ensureLoggedIn() {
-    // Check that we're looking at the logged in homepage and throw an
-    // error if we aren't.
-    await u.wait(this.page, '.accounts-body');
+    //await u.wait(this.page, '.accounts-body');
+    // using this selector for site redesign, but not confirmed its appropriate yet
+    await u.wait(this.page, 'div.c-section.c-section--primary');
     this.logged_in = true;
   }
 
@@ -176,7 +176,9 @@ class Session {
 
   async home() {
     await u.click(this.page, '[aria-label="Home"]');
-    await u.wait(this.page, '.accounts-body');
+
+    //await u.wait(this.page, '.accounts-body');
+    await u.wait(this.page, 'div.c-section.c-section--primary');
   }
 }
 
