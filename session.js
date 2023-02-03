45026fc01514e44aeeec2b79315137d51240878d
carpii <carpii.uk@gmail.com>
2023-02-03 21:26:01 +0000

    parameterise login selector

--- session.js
+++ session.js
@@ -3,6 +3,10 @@ const u = require('./utils.js');
 const Account = require('./account.js');
 
 class Session {
+  // .accounts_body - used for business banking and pre-2023 personal banking
+  // div.c-section.c-section--primary - used for post-2023 personal banking
+  let selector_IsLoggedIn = '.accounts-body, div.c-section.c-section--primary';
+
   async init(options) {
     this.browser = await puppeteer.launch(options);
     this.page = await this.browser.newPage();
@@ -51,9 +55,8 @@ class Session {
   }
 
   async ensureLoggedIn() {
-    //await u.wait(this.page, '.accounts-body');
     // using this selector for site redesign, but not confirmed its appropriate yet
-    await u.wait(this.page, 'div.c-section.c-section--primary');
+    await u.wait(this.page, selector_IsLoggedIn);
     this.logged_in = true;
   }
 
@@ -177,8 +180,8 @@ class Session {
   async home() {
     await u.click(this.page, '[aria-label="Home"]');
 
-    //await u.wait(this.page, '.accounts-body');
-    await u.wait(this.page, 'div.c-section.c-section--primary');
+    // using this selector for site redesign, but not confirmed its appropriate yet
+    await u.wait(this.page, selector_IsLoggedIn);
   }
 }
 
